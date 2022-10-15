43624073524841390d583a4f4574cac288eceb74
Nemanja Lekic <nemanjalekic.ns@gmail.com>
2022-10-15 17:09:02 +0200

    Update docker-entrypoint.sh
    Collect static files

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,5 +4,9 @@
 echo "Apply database migrations"
 python manage.py migrate
 
+# Collect static files
+echo "Collecting static files"
+python manage.py collectstatic
+
 #Start server
 gunicorn app.wsgi:application --bind 0.0.0.0:80
