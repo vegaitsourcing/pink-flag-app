557dc4667686306868f4d20faf9aea2edff9fd14
VeskoDev <lazarevic.vesko@gmail.com>
2022-10-15 20:07:07 +0200

    Add a calendar module and donations module

--- backend/app/blog/models/__init__.py
+++ backend/app/blog/models/__init__.py
@@ -3,3 +3,7 @@ from .blog_index_page import BlogIndexPage
 from .donation_page import DonationPage
 from .footer_page import FooterPage
 from .about_us_page import AboutUsPage
+from .donate_module import DonationsModule
+from .calendar_module import CalendarModule
+
+
